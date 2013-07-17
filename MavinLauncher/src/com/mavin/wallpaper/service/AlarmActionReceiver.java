69d41e7d47e1facf8507f5de738cb30ac04db1d0
Anshu Gaind <Anshu.Gaind@Gmail.com>
2013-07-16 20:01:36 -0500

    streamlined wallpaper service, sdk 1.9 support, commented out extra permissions and added an additional permission in the manifest

--- MavinLauncher/src/com/mavin/wallpaper/service/AlarmActionReceiver.java
+++ MavinLauncher/src/com/mavin/wallpaper/service/AlarmActionReceiver.java
@@ -1,43 +1,23 @@
 package com.mavin.wallpaper.service;
 
-import java.io.File;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.util.Log;
 
-import com.mavin.ad.AdHandler;
-import com.mavin.ad.ImageAd;
-import com.mavin.ads.MavinAdActivity;
 import com.mavin.util.Utils;
 
 public class AlarmActionReceiver extends BroadcastReceiver {
-	private static final String TAG = AlarmActionReceiver.class.getSimpleName();
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Log.d(TAG, "Entering onReceive()::" + intent.getAction());
+		Utils.log("Entering onReceive()::" + intent.getAction());
 
 		if (intent.getAction().equals("com.mavin.wallpaper.ALARM_ACTION")) {
-			Log.d(TAG, "com.mavin.wallpaper.ALARM_ACTION broadcast received!");
-			startWallpaperService(context);
+			Utils.log("com.mavin.wallpaper.ALARM_ACTION broadcast received!");
+			Intent service = new Intent(context, WallpaperService.class);
+			context.startService(service);
 		}
-		Log.d(TAG, "Exiting onReceive()::" + intent.getAction());
+		Utils.log("Exiting onReceive()::" + intent.getAction());
 
 	}
-
-	/**
-	 * 
-	 * @param context
-	 */
-	private void startWallpaperService(Context context) {
-		Log.d(TAG, "Entering startWallpaperService()");
-		Intent intent = new Intent(context, WallpaperService.class);
-		context.startService(intent);
-		Log.d(TAG, "Exiting startWallpaperService()");
-	}
-
-
 }
--- MavinLauncher/src/com/mavin/wallpaper/service/WallpaperService.java
+++ MavinLauncher/src/com/mavin/wallpaper/service/WallpaperService.java
@@ -3,16 +3,15 @@ package com.mavin.wallpaper.service;
 
 import java.io.File;
 
-import android.app.Activity;
+import android.app.IntentService;
 import android.app.Service;
 import android.app.WallpaperManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.os.IBinder;
-import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
 
 import com.mavin.ad.AdHandler;
 import com.mavin.ad.AdKey;
@@ -21,74 +20,41 @@ import com.mavin.ad.ImageAd;
 import com.mavin.util.ConfigurationManager;
 import com.mavin.util.Utils;
 
-public class WallpaperService extends Service {
-    private static String TAG;
+public class WallpaperService extends IntentService {
+    private static String TAG = WallpaperService.class.getSimpleName();
 
-    private Service mService;
-    private SharedPreferences mPreferences;
-    private String mDeviceId;
-    private boolean isRunning = false;
     private static boolean USE_SCALED_BITMAP;
 
-    @Override
-    public IBinder onBind(Intent intent)
-    {
-        return null;
+    public WallpaperService() {
+        super("WallpaperService");
+    }
+
+    public WallpaperService(String name) {
+        super(name);
     }
 
     @Override
-    public void onCreate() {
-        // setup TAG
-        TAG = this.getClass().getSimpleName();
-        mService = this;
-        mPreferences = getSharedPreferences("mavin",
-                Activity.MODE_PRIVATE);
-        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        mDeviceId = telephonyManager.getDeviceId();
+    protected void onHandleIntent(Intent arg0) {
+        Utils.log("Entering");
+        setWallpaper();
+        Utils.log("Exiting");
     }
 
-    @Override
-    public void onDestroy() {
-        // TODO Auto-generated method stub
-        super.onDestroy();
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i(TAG, "onStartCommand");
+    public void setWallpaper() {
+        Utils.log("Entering");
         try {
             String useScaledBitmap = ConfigurationManager.getInstance(
                     new File(Utils.getStorageDirectory() + File.separator
                             + "config.properties")).getProperty(
                     "mavin.wallpaper.use.scaled.bitmap");
-            USE_SCALED_BITMAP = useScaledBitmap
-                    .equalsIgnoreCase("Y") ? true : false;
-            final int startId_ = startId;
-            if (!isRunning)
-            {
-                isRunning = true;
-            } else
-            {
-                Log.d(TAG,
-                        "Wallpaper service is already running, stop and return!!!");
-                // For whatever reason, the previous onStartCommand is still
-                // running,
-                // perhaps the service got
-                // killed by runtime and this service is not STICKY, so it never
-                // reach end
-                // to call stopSelf?
-                // So just kill it and let another alarm to start it again.
-                stopSelf(startId_);
-                return Service.START_NOT_STICKY;
-            }
-            if ((flags & Service.START_FLAG_RETRY) == Service.START_FLAG_RETRY)
-            {
-                Log.d(TAG, "Wallpaper service retry");
-            }
+            USE_SCALED_BITMAP = useScaledBitmap.equalsIgnoreCase("Y") ? true
+                    : false;
 
             WallpaperManager _wallpaperManager = WallpaperManager
                     .getInstance(getApplicationContext());
             if (_wallpaperManager != null) {
+
+                setWallpaperDimension();
 
                 AdKey key = new AdKey();
                 key.setApplication(ApplicationType.WALLPAPER.getType());
@@ -100,40 +66,57 @@ public class WallpaperService extends Service {
                 if (_imageAd != null) {
                     Bitmap useThisBitmap = null;
                     if (USE_SCALED_BITMAP) {
-                        Log.i(TAG, " Setting Wallpaper: using scaled image " + _imageAd.getUri());
-                        useThisBitmap = Bitmap.createScaledBitmap(
-                                Utils.getBitmap(new File(Utils.getStorageDirectory()
-                                        + File.separator
-                                        + _imageAd.getUri())),
+                        Log.i(TAG, " Setting Wallpaper: using scaled image "
+                                + _imageAd.getUri());
+                        useThisBitmap = Bitmap.createScaledBitmap(Utils
+                                .getBitmap(new File(Utils.getStorageDirectory()
+                                        + File.separator + _imageAd.getUri())),
                                 _wallpaperManager.getDesiredMinimumWidth(),
-                                _wallpaperManager.getDesiredMinimumHeight(), true);
+                                _wallpaperManager.getDesiredMinimumHeight(),
+                                true);
                     } else {
-                        useThisBitmap = Bitmap.createBitmap(Utils.getBitmap(new File(Utils
-                                .getStorageDirectory() + File.separator
-                                + _imageAd.getUri())));
+                        useThisBitmap = Bitmap.createBitmap(Utils
+                                .getBitmap(new File(Utils.getStorageDirectory()
+                                        + File.separator + _imageAd.getUri())));
                     }
                     _wallpaperManager.setBitmap(useThisBitmap);
-                    Log.i(TAG, " Setting Wallpaper: " + _imageAd.getUri());
+                    Utils.log("Setting Wallpaper: " + _imageAd.getUri());
 
                     // broadcast the ad that was displayed
                     Intent _message = new Intent();
                     _message.setAction("com.android.wallpaper.AD_DISPLAYED_ACTION");
                     sendBroadcast(_message);
                 } else {
-                    Log.w(TAG, "No Ad Found");
+                    Utils.warn("No Ad Found");
                 }
             } else {
-                Log.w(TAG, "WallpaperManager Not Found");
+                Utils.warn("WallpaperManager Not Found");
             }
 
-        } catch (Throwable e)
-        {
+        } catch (Throwable e) {
             Log.e(TAG,
                     "error: "
                             + ((e.getMessage() != null) ? e.getMessage()
                                     .replace(" ", "_") : ""), e);
+        } finally {
+            Utils.log("Exiting");
         }
-        return Service.START_NOT_STICKY;
 
     }
+
+    protected void setWallpaperDimension() {
+        Utils.log("Entering");
+        WallpaperManager _wallpaperManager = WallpaperManager
+                .getInstance(getApplicationContext());
+        if (_wallpaperManager != null) {
+            WindowManager window = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+            Display display = window.getDefaultDisplay();
+            int width = display.getWidth();
+            int height = display.getHeight();
+            _wallpaperManager.suggestDesiredDimensions(width, height);
+            Utils.log("done suggestDesiredDimensions()");
+        }
+        Utils.log("Exiting");
+    }
+
 }
